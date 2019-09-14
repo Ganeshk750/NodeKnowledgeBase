2dd8688e3a243518dc442e8d18c85853975d16ca
Ganeshk750 <ganesh9ot@gmail.com>
2019-09-14 18:25:36 +0530

    Creat Model and insert some data and fetch that data

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+//check connection
+db.once('open', () =>{
+    console.log('Connected MongoDb Successfully!!');
+});
+//check err
+db.on('error', (err)=>{
+    console.log(err);
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) =>{
-   // res.send('Hello World!!');
-   let articles = [
+app.get("/", (req, res) => {
+  // res.send('Hello World!!');
+  /*   let articles = [
        {id: 1, title: 'This is firstOne', author: 'authorOne'},
        {id: 2, title: 'This is SecondOne', author: 'SecondOne'},
        {id: 3, title: 'This is ThirdOne', author: 'ThirdOne'},
        {id: 4, title: 'This is FourthOne', author: 'FourthOne'},
-   ];
-   res.render('index',{
-       title: 'Articles',
-       articles: articles
-   });
+   ]; */
+  Article.find({}, (err, articles) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", {
+        title: "Articles",
+        articles: articles
+      });
+    }
+  });
 });
 
 app.get('/add/articles', (req, res) =>{
