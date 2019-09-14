2c38e65f0273b331383920de0ed7ef933f238d71
Ganeshk750 <ganesh9ot@gmail.com>
2019-09-14 17:13:13 +0530

    adding templete engine

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) =>{
-    res.send('Hello World!!');
-})
+   // res.send('Hello World!!');
+   let articles = [
+       {id: 1, title: 'This is firstOne', author: 'authorOne'},
+       {id: 2, title: 'This is SecondOne', author: 'SecondOne'},
+       {id: 3, title: 'This is ThirdOne', author: 'ThirdOne'},
+       {id: 4, title: 'This is FourthOne', author: 'FourthOne'},
+   ];
+   res.render('index',{
+       title: 'Articles',
+       articles: articles
+   });
+});
+
+app.get('/add/articles', (req, res) =>{
+    res.render('add_articles',{
+        title: 'Add Articles'
+    });
+ });
 
 
-app.listen(3000, () => console.log('Server is runnning on port 3000....'));
+app.listen(3000, () => console.log('Server is running on port 3000....'));
